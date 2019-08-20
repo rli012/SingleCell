84bf4a75619389fa41925df4308319bf24233de2
Ruidong Li <rli012@ucr.edu>
2019-08-19 19:36:29 -0700

    Update and rename SGE_qsub_S5_S1_GEX.sh to SGE_CellRanger_S1_GEX.sh

--- SGE_CellRanger_S1_GEX.sh
+++ SGE_CellRanger_S1_GEX.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
-#$ -N S5_S1_GEX
+#$ -N S1_GEX
 # #$ -V # Job has the same environment variables as the submission shell
-#$ -l h=fccccappdevn03
-#$ -pe threads 8 # -pe peomics 2 ??? # -pe threads
+#$ -l h=highmem
+#$ -pe peomics 8 # -pe threads
 #$ -l mem_free=8G # h_vmem=4G hard limit of the maximum amount of vitual memory
-#$ -o "logs/S5_S1_GEX.stdout"
-#$ -e "logs/S5_S1_GEX.stderr"
-#$ -wd "/home/rli3/Projects/HPK1_Single_Cell_RNAseq_20190812/data/CellRanger/"
+#$ -o "logs/S1_GEX.stdout"
+#$ -e "logs/S1_GEX.stderr"
+#$ -wd "/data/CellRanger/"
 # #$ -cwd
 
 # #$ -v "REF=hg38,FQ=test.fq,SAM=test.sam" # no space!!!
 # #$ -l h_rt=00:02:00
-# #$ -wd "/home/rli3/Documents"
 # #$ -t 1-3 # task array
 
-# usage: qsub SGE_qsub_template.sh
+# usage: qsub SGE_CellRanger_S1_GEX.sh
 # OR
-# usage: qsub -N run_test -V -pe threads 2 -l h_vmem=4G -o "logs/test.stdout" -e "logs/test.stderr" SGE_qsub_template.sh
-
-CPU=$NSLOTS
+# usage: qsub -N S1_GEX -V -pe peomics 8 -l mem_free=8G -o "logs/S1_GEX.stdout" -e "logs/S1_GEX.stderr" SGE_CellRanger_S1_GEX.sh
 
 echo 'Start'
-#echo $CPU
-
-#echo ${REF}
-#echo $FQ
-#echo $SAM
-
 cellranger count --id=S1_GEX \
-		 --fastqs=/resbioinfo/data/MedGenome/P2000413_07292019/FASTQ \
+		 --fastqs=/data/FASTQ \
 		 --transcriptome=/home/rli3/bin/refdata-cellranger-GRCh38-3.0.0 \
 		 --sample=S1_GEX \
 		 --localcores=8
@@ -36,11 +27,14 @@ cellranger count --id=S1_GEX \
 echo 'Done'
 
 
-
 #:<<SKIP
 
 ### TASK ARRAY
 #N=$SGE_TASK_ID
+
+#echo $CPU
+
+CPU=$NSLOTS
 
 #FILE=`ls raw/test*\.fastq.gz | grep _1.fastq.gz | head -n $N | tail -n 1`
 #SAMPLE=${FILE%_1.fastq.gz}
@@ -50,6 +44,11 @@ echo 'Done'
 
 
 #echo 'Start'
+
+
+#echo ${REF}
+#echo $FQ
+#echo $SAM
 
 #echo $fq1
 #echo $fq2
