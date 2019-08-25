7dba211d47a0b956bee8211a508a5a2e187c2082
Ruidong Li <rli012@ucr.edu>
2019-08-25 15:23:54 -0700

    Update singleR_Annotation.R

--- singleR_Annotation.R
+++ singleR_Annotation.R
@@ -108,15 +108,37 @@ plot_grid(p3, p4)
 
 
 
-
 ########################################
 ### Create Reference
 
+name = 'FANTOM5'
+expr = as.matrix(expr) # the expression matrix
+types = as.character(types) # a character list of the types. Samples from the same type should have the same name.
+main_types = as.character(main_types) # a character list of the main types. 
+fantom5 = list(name=name,
+               data = expr, 
+               types=types, 
+               main_types=main_types)
+
+# if using the de method, we can predefine the variable genes
+fantom5$de.genes = CreateVariableGeneSet(expr,types,200)
+fantom5$de.genes.main = CreateVariableGeneSet(expr,main_types,300)
+
+# if using the sd method, we need to define an sd threshold
+sd = rowsSd(expr)
+sd.thres = sort(sd, decreasing = T)[4000] # or any other threshold
+fantom5$sd.thres = sd.thres
+
+saveRDS(fantom5,file='SingleR_FANTOM5_Reference.rds') 
+# it is best to name the object and the file with the same name.
 
 
+###########
+fantom5 <- readRDS(file='SingleR_FANTOM5_Reference.rds') 
 
-
-
-
-
-
+### Single cell
+singlerCellAnnotation = CreateSinglerObject(seurat.integrated[['RNA']]@counts, annot = NULL, project.name='PBMC', 
+                                            min.genes = 0, technology = "10X", species = "Human", citation = "",
+                                            ref.list = list(fantom5), normalize.gene.length = F, variable.genes = "de",
+                                            fine.tune = T, do.signatures = T, clusters = NULL, do.main.types = T, 
+                                            reduce.file.size = T, numCores = 4)
