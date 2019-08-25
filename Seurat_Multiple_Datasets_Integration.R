f4c3143c4d20631c66ff35e0fbbefc61abdaf795
Ruidong Li <rli012@ucr.edu>
2019-08-25 10:10:54 -0700

    Add files via upload

--- Seurat_Multiple_Datasets_Integration.R
+++ Seurat_Multiple_Datasets_Integration.R
@@ -1,56 +1,89 @@
+setwd('C:\\Users/rli3/Desktop/share/BioinfoHub/')
+
+#library(devtools)
+#devtools::install_github(repo = "satijalab/seurat", ref = "develop",
+#                         INSTALL_opts = c('--no-lock'))
+
+library(Seurat)
+library(ggplot2)
+library(cowplot)
+library(Matrix)
+source('Single_Cell_Functions.R')
+
+
+#################################################################
+######## STEP 1. Read Raw Data
+
+fds <- list.dirs('data/CellRanger')
+fds <- fds[grep('filtered_feature_bc_matrix', fds)]
+fds
+
+num.samples <- length(fds)
 
 metadata <- c()
 mat.list <- list()
 
-for (i in 1:4) {
-  fd <- fds[i+1]
-  
-  sam = strsplit(fd, '/')[[1]][3]
-  
+for (i in 1:num.samples) {
+  matrixDir <- fds[i]
+  sam = strsplit(matrixDir, '/')[[1]][3]
   print (sam)
-  
-  matrixDir <- fd
   
   mat.list[[sam]] <- Read10XRL(data.dir = matrixDir)
   colnames(mat.list[[sam]]) <- gsub('(\\w+)-1', paste0(i, '_\\1'), colnames(mat.list[[sam]]))
   
-  metadata <- rbind(metadata, data.frame(barcode=colnames(mat.list[[sam]]), celltype=sam))
+  metadata <- rbind(metadata, data.frame(barcode=colnames(mat.list[[sam]]), sample=sam))
 
 }
 
+metadata[1:5,]
 
+
+
+##################################################################
+####### STEP 2. Create Seurat Object, Cell QC, Data Normalization
+
+min.cells <- 5
 seurat.list <- list()
 
 for (i in 1:length(mat.list)) {
-  seurat.list[[i]] <- CreateSeuratObject(counts = mat.list[[i]], min.cells = 5)
-  seurat.list[[i]][["percent.mt"]] <- PercentageFeatureSet(seurat.list[[i]], pattern = "^MT-")
+  seurat.list[[i]] <- CreateSeuratObject(counts = mat.list[[i]], min.cells = min.cells)
   seurat.list[[i]]@meta.data$sample <- names(mat.list)[i]
+  seurat.list[[i]]$percent_mt <- PercentageFeatureSet(seurat.list[[i]], pattern = "^MT-")
   
-  p <- VlnPlot(seurat.list[[i]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+  p <- VlnPlot(seurat.list[[i]], features = c("nFeature_RNA", "nCount_RNA", "percent_mt"), ncol = 3)
   
   png(filename = paste0('report/Cell_QC_', names(mat.list)[i], '.png'), width = 700, height = 500)
   print (p)
   dev.off()
   
-  seurat.list[[i]] <- subset(seurat.list[[i]], subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+  seurat.list[[i]] <- subset(seurat.list[[i]], subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent_mt < 5)
   
   message(paste0(ncol(seurat.list[[i]][['RNA']]@counts), '/', ncol(mat.list[[i]]), ' of cells are kept'))
   
-  seurat.list[[i]] <- NormalizeData(seurat.list[[i]], verbose = TRUE)
+  seurat.list[[i]] <- NormalizeData(seurat.list[[i]], normalization.method = "LogNormalize",
+                                    scale.factor = 10000,verbose = TRUE)
   seurat.list[[i]] <- FindVariableFeatures(seurat.list[[i]], selection.method = "vst", 
                                            nfeatures = 2000, verbose = TRUE)
+  
+  seurat.list[[i]] <- SCTransform(seurat.list[[i]], vars.to.regress = "percent_mt", 
+                                  verbose = TRUE)
+  
 }
+names(seurat.list) <- names(mat.list)
+seurat.list[[i]]@meta.data[1:5,]
 
 
 dataForBarPlot <- c()
-for (i in 1:4) {
+for (i in 1:num.samples) {
   dataForBarPlot <- rbind(dataForBarPlot, 
                           c(ncol(mat.list[[i]]), ncol(seurat.list[[i]][['RNA']]@counts)))
 }
+dataForBarPlot
+
 
 dataForBarPlot <- data.frame(freq=unlist(c(dataForBarPlot)),
-                             group=rep(c('Before QC','After QC'), each=4),
-                             sample=rep(c('S1_GEX','S2_GEX','S3_GEX','S4_GEX'), 4))
+                             group=rep(c('Before QC','After QC'), each=num.samples),
+                             sample=rep(names(mat.list), num.samples))
 
 dataForBarPlot$group <- factor(dataForBarPlot$group, levels=c('Before QC','After QC'))
 
@@ -85,17 +118,31 @@ ggplot(data=dataForBarPlot, aes(x=sample, y=freq,
 
 
 ####
+names(seurat.list)
+DefaultAssay(seurat.list[[1]])
 
-names(seurat.list) <- c('S1','S2','S3','S4')
+reference.list <- seurat.list[c('S1_GEX','S2_GEX')]
+seurat.features <- SelectIntegrationFeatures(object.list = reference.list, nfeatures = 3000)
+reference.list <- PrepSCTIntegration(object.list = reference.list, anchor.features = seurat.features, 
+                                  verbose = TRUE)
 
-reference.list <- seurat.list[c('S1','S2','S3','S4')]
+seurat.anchors <- FindIntegrationAnchors(object.list = reference.list, normalization.method = "SCT", 
+                                       anchor.features = seurat.features)
+seurat.integrated <- IntegrateData(anchorset = seurat.anchors, normalization.method = "SCT")
+
+
+
+#================================================================================
+###
+reference.list <- seurat.list[c('S1_GEX','S2_GEX')]
 seurat.anchors <- FindIntegrationAnchors(object.list = reference.list, dims = 1:30)
-
-
 seurat.integrated <- IntegrateData(anchorset = seurat.anchors, dims = 1:30)
+seurat.integrated <- ScaleData(seurat.integrated, verbose = FALSE)
+
 #?IntegrateData()
 ?MergeSeurat()
 
+### No Batch Correction
 #seurat.integrated <- merge(x=seurat.list[[1]], 
 #                           y=c(seurat.list[[2]],seurat.list[[3]],seurat.list[[4]],
 #                               seurat.list[[5]],seurat.list[[6]],seurat.list[[7]],
@@ -105,37 +152,46 @@ seurat.integrated <- IntegrateData(anchorset = seurat.anchors, dims = 1:30)
 #seurat.integrated <- NormalizeData(seurat.integrated, verbose = TRUE)
 #seurat.integrated
 
+#================================================================================
+
+
+
 # switch to integrated assay. The variable features of this assay are automatically
 # set during IntegrateData
 names(seurat.integrated)
 
-dim(seurat.integrated[['RNA']]@counts)
-dim(seurat.integrated[['integrated']]@counts)
+dim(seurat.integrated[['RNA']]@data)
+dim(seurat.integrated[['integrated']]@scale.data)
 
-DefaultAssay(seurat.integrated) <- "integrated"
+seurat.integrated[['RNA']]@data[1:10,1:10]
+seurat.integrated[['RNA']]@data[1:10,1:10] == seurat.list[[1]][['RNA']]@data[1:10,1:10]
+
+DefaultAssay(seurat.integrated)
+if (DefaultAssay(seurat.integrated) != 'integrated') {
+  DefaultAssay(seurat.integrated) <- "integrated"
+}
 
 
 # Run the standard workflow for visualization and clustering
-seurat.integrated <- ScaleData(seurat.integrated, verbose = FALSE)
 seurat.integrated <- RunPCA(seurat.integrated, npcs = 30, verbose = FALSE)
-
-seurat.integrated <- FindNeighbors(seurat.integrated, dims = 1:30)
-seurat.integrated <- FindClusters(seurat.integrated, resolution = 0.5)
 
 # Look at cluster IDs of the first 5 cells
 head(Idents(seurat.integrated), 5)
 seurat.integrated@meta.data
 
-seurat.integrated <- RunTSNE(seurat.integrated, reduction = "pca", dims = 1:30)
+seurat.integrated <- RunTSNE(seurat.integrated, reduction = "pca", dims = 1:30, seed.use = '777')
+seurat.integrated <- RunUMAP(seurat.integrated, reduction = "pca", dims = 1:30, seed.use = '777')
 
-seurat.integrated <- RunUMAP(seurat.integrated, reduction = "pca", dims = 1:30)
+seurat.integrated <- FindNeighbors(seurat.integrated, dims = 1:30)
+seurat.integrated <- FindClusters(seurat.integrated, resolution = 0.5)
 
-saveRDS(seurat.integrated, file='data/rData/Seurat_Integrated_4_Samples_Cell_QC_PCA_tSNE_Clustering.rds')
+#saveRDS(seurat.integrated, file='data/rData/Seurat_Integrated.rds')
+seurat.integrated@meta.data[1:5,]
 
 p1 <- DimPlot(seurat.integrated, reduction = "tsne", group.by = "sample")
-p1 <- DimPlot(seurat.integrated, reduction = "tsne", group.by = "seurat_clusters")
-p2 <- DimPlot(seurat.integrated, reduction = "umap", group.by = "celltype", label = TRUE, 
-              repel = TRUE) + NoLegend()
+p2 <- DimPlot(seurat.integrated, reduction = "tsne", group.by = "seurat_clusters")
+#p2 <- DimPlot(seurat.integrated, reduction = "umap", group.by = "celltype", label = TRUE, 
+#              repel = TRUE) + NoLegend()
 plot_grid(p1, p2)
 
-p1
+
